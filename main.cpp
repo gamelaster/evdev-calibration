76b4f39b241af9d0a6f813776bfba4ad780d5a29
Marek Kraus <gamelaster@outlook.com>
2018-01-24 10:48:01 +0100

    Added some changes

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <fstream>
 
 static int IsEventDevice(const struct dirent *dir) {
 	return strncmp("event", dir->d_name, 5) == 0;
@@ -57,8 +59,8 @@ int main(int argc, char *argv[])
 	printf("Max X: %6d, Max Y: %6d\n", maxXpos, maxYpos);
 
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
-		fprintf(stderr, "Unable to initialize SDL: %s", SDL_GetError());
-        return 1;
+		fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
+		return 1;
 	}
 	SDL_DisplayMode displayMode;
 	SDL_GetDesktopDisplayMode(0, &displayMode);
@@ -67,35 +69,35 @@ int main(int argc, char *argv[])
 	SDL_Window* window = SDL_CreateWindow(
 		"EVDEV Calibration",
 		SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        displayMode.w,
-        displayMode.h,
-        SDL_WINDOW_FULLSCREEN 
+		SDL_WINDOWPOS_UNDEFINED,
+		displayMode.w,
+		displayMode.h,
+		SDL_WINDOW_FULLSCREEN 
 	);
 
 
 
 	if (window == NULL) {
-        fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
-        return 2;
-    }
+		fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
+		return 2;
+	}
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-    if (renderer == NULL) {
-    	fprintf(stderr, "Could not create renderer %s\n", SDL_GetError());
-    	return 3;
-    }
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	if (renderer == NULL) {
+		fprintf(stderr, "Could not create renderer %s\n", SDL_GetError());
+		return 3;
+	}
 
-    bool isRunning = true;
+	bool isRunning = true;
 
 	const size_t ev_size = sizeof(struct input_event);
 	struct input_event ev;
-    ssize_t size;
+	ssize_t size;
 
-    int corner = 0, touchScreenWidth, touchScreenHeight, touchAreaXoffset, touchAreaYoffset;
-    int lastX, lastY;
+	int corner = 0, touchScreenWidth, touchScreenHeight, touchAreaXoffset, touchAreaYoffset;
+	int lastX, lastY;
 
-    while (isRunning) {
+	while (isRunning) {
 		SDL_SetRenderDrawColor(renderer, 176, 224, 230, 255);
 		SDL_RenderClear(renderer);
 
@@ -132,39 +134,39 @@ int main(int argc, char *argv[])
 
 		}
 
-        SDL_RenderPresent(renderer);
+		SDL_RenderPresent(renderer);
 
-        size = read(fd, &ev, ev_size);
-        if (size < ev_size) {
-            fprintf(stderr, "Error size when reading\n");
-            return 1;
-        }
+		size = read(fd, &ev, ev_size);
+		if (size < ev_size) {
+			fprintf(stderr, "Error size when reading\n");
+			return 1;
+		}
 
-        if (ev.type == EV_ABS) {
-            if (ev.code == ABS_X) {
-            	lastX = ev.value;
-            }
-            else if (ev.code == ABS_Y) {
-            	lastY = ev.value;
-            }
-        }
-        else if (ev.type == EV_KEY && ev.code == BTN_LEFT && ev.value == 0) {
-        	switch (corner) {
-        		case 0:
-        			touchAreaXoffset = (maxXpos * 0.2) - lastX;
-        			touchAreaYoffset = (maxYpos * 0.2) - lastY;
-        			break;
-        		case 2:
-        			touchScreenWidth = maxXpos - ((lastX - (maxXpos * 0.8)) * 0.5);
-        			touchScreenHeight = maxYpos - ((lastY - (maxYpos * 0.8)) * 0.5);
-        			break;
-        	}
-        	printf("%i: X: %i, Y: %i\n", corner, lastX, lastY);
-        	corner++;
-        }
-        if (corner == 4) {
-        	printf("Width: %d, Height: %d\nOffset X: %d, Offset Y: %d\n", touchScreenWidth, touchScreenHeight, touchAreaXoffset, touchAreaYoffset);
-        	float c0 = (float)touchScreenWidth / (float)maxXpos;
+		if (ev.type == EV_ABS) {
+			if (ev.code == ABS_X) {
+				lastX = ev.value;
+			}
+			else if (ev.code == ABS_Y) {
+				lastY = ev.value;
+			}
+		}
+		else if (ev.type == EV_KEY && ev.code == BTN_LEFT && ev.value == 0) {
+			switch (corner) {
+				case 0:
+					touchAreaXoffset = (maxXpos * 0.2) - lastX;
+					touchAreaYoffset = (maxYpos * 0.2) - lastY;
+					break;
+				case 2:
+					touchScreenWidth = maxXpos - ((lastX - (maxXpos * 0.8)) * 0.5);
+					touchScreenHeight = maxYpos - ((lastY - (maxYpos * 0.8)) * 0.5);
+					break;
+			}
+			printf("%i: X: %i, Y: %i\n", corner, lastX, lastY);
+			corner++;
+		}
+		if (corner == 4) {
+			printf("Width: %d, Height: %d\nOffset X: %d, Offset Y: %d\n", touchScreenWidth, touchScreenHeight, touchAreaXoffset, touchAreaYoffset);
+			float c0 = (float)touchScreenWidth / (float)maxXpos;
 			float c2 = (float)touchScreenHeight / (float)maxYpos;
 			float c1 = (float)touchAreaXoffset / (float)maxXpos;
 			float c3 = (float)touchAreaYoffset / (float)maxYpos;
@@ -172,18 +174,29 @@ int main(int argc, char *argv[])
 			ioctl(fd, EVIOCGNAME(sizeof(deviceName)), deviceName);
 			char xinputCommand[256];
 			snprintf(xinputCommand, sizeof(xinputCommand), "xinput set-prop '%s' 'Coordinate Transformation Matrix' %.6f 0 %.6f 0 %.6f %.6f 0 0 1", deviceName, c0, c1, c2, c3);
-			printf("Execute this: \"%s\"\n", xinputCommand);
-			system(xinputCommand);
+			printf("Do you want to apply configuration? [Y/n]: ");
+			char applyCalibration;
+			scanf(" %c", &applyCalibration);
+			if (applyCalibration == 'Y' || applyCalibration == 'y') {
+				system(xinputCommand);
+			}
+			char xorgFile[512];
+			snprintf(xorgFile, sizeof(xorgFile), "Section \"InputClass\"\n"
+			"\tIdentifier      \"calibration\"\n"
+			"\tMatchProduct    \"%s\"\n"
+			"\tOption \"TransformationMatrix\" \"%.6f 0 %.6f 0 %.6f %.6f 0 0 1\"\n"
+			"EndSection", deviceName, c0, c1, c2, c3);
+			printf("Please, write this configuration to: /usr/share/X11/xorg.conf.d/99-calibration.conf\n%s\n", xorgFile);
 			isRunning = false;
-        }
-        SDL_Delay(1000 / 64);
-    }
+		}
+		SDL_Delay(1000 / 64);
+	}
 
 	close(fd);
 
-    SDL_DestroyWindow(window);
-    SDL_DestroyRenderer(renderer);
-    SDL_Quit();
-    return 0;
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+	return 0;
 
 }
